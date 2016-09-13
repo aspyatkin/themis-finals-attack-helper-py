c7f05d26059b106c6273c8fd856657ee59abf89b
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-09-13 11:58:54 +0000

    Draft

---
+++ setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import io
+import os
+
+
+about = {}
+about_filename = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    'themis', 'finals', 'attack', 'helper', '__about__.py')
+with io.open(about_filename, 'rb') as fp:
+    exec(fp.read(), about)
+
+
+setup(
+    name='themis.finals.attack.helper',
+    version=about['__version__'],
+    description='Themis Finals attack helper library',
+    author='Alexander Pyatkin',
+    author_email='aspyatkin@gmail.com',
+    url='https://github.com/aspyatkin/themis-finals-attack-helper-py',
+    license='MIT',
+    packages=find_packages('.'),
+    install_requires=[
+        'setuptools>=0.8',
+        'requests>=2.11.1',
+        'themis.finals.attack.result>=1.0.0,<1.1.0'
+    ],
+    namespace_packages=[
+        'themis',
+        'themis.finals',
+        'themis.finals.attack'
+    ],
+    entry_points={
+        'console_scripts': [
+            'themis-finals-attack = themis.finals.attack.helper:run',
+        ]
+    }
+)
